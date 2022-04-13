2956a277a51c373cadc766c6855ae65c94ef3b4e
Shrikanth <shrikanthv00@gmail.com>
2022-04-13 14:59:27 +0530

    final

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -4,13 +4,13 @@
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 (function(window){
-	var byeSpeaker = {};
+	var byeSpeaker = {}
 	var speakWord = "Good Bye";
 	byeSpeaker.speak = function (name){
 		console.log(speakWord + " " + name);
 	}
     window.byeSpeaker = byeSpeaker;
-}(window); 
+})(window); 
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -1,7 +1,7 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 (function(window){
-	var helloSpeaker={};
+	var helloSpeaker={}
 	var speakWord = "Hello";
 	helloSpeaker.speak = function (name) {
 		console.log(speakWord + " " + name);
